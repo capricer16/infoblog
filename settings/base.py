2a517197fa88a32920d0ee23c67a5e1d3c447540
cristianbufalini <cristianbufaliniadrian@gmail.com>
2022-08-02 11:15:38 -0300

    agregando configuraciones de settings manage.py y app accounts

--- settings/base.py
+++ settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts',
+    
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
