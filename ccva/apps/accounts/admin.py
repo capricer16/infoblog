f66a51655c079da06e21f60863b09dc69cc7d110
joafran <joafran0016@gmail.com>
2022-08-12 14:27:35 -0300

    Models

--- ccva/apps/accounts/admin.py
+++ ccva/apps/accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import Register, Comment, CyC
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Register, Comment, CyC)
