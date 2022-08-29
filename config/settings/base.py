663a44c5a03b429cc9a1035896f624f0506fcb6a
Fabiana Caprice Ramirez <fabianacapriceramirez@gmail.com>
2022-08-29 15:58:36 -0300

    Agregue programas

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
         'NAME': 'infoblogbd',
         'USER': 'postgres',
         'PASSWORD': 'ccva2022',
--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,8 @@
+import dj_database_url
 from .base import *
 
 
+
+
 ALLOWED_HOSTS = ['*.herokuapp.com', 'dev-infoblog.herokuapp.com']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
